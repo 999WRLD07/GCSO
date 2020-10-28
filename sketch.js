3e83aaeaa82f1a37683fac68c3055d347330ff25
999WRLD07 <72419000+999WRLD07@users.noreply.github.com>
2020-10-28 19:06:47 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var speed,weight;
 function setup() {
   createCanvas(1600,400);
  car = createSprite(50, 200, 50, 50);
- car.velocityX = 0;
+ car.velocityX = speed;
  
  wall = createSprite(1500, 200, 60, height/2);
 
@@ -16,7 +16,7 @@ weight=random(400,1500);
 function draw() {
   background(80,80,80);  
   
-if(wall.x=car.x <(car.width+wall.width)/2)
+if(wall.x-car.x <(car.width+wall.width)/2)
 {
 
 car.velocityX=0;
